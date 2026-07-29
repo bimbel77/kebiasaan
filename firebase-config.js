2199510d96dfb341157feb7c89b57686c73683bb
seputar guru <zakariahc81@gmail.com>
2026-07-29 20:17:06 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCODPfjAf4gvjQ4OG1HkzSpWOnNzZH13bA",
@@ -12,7 +12,7 @@ const firebaseConfig = {
   measurementId: "G-YVPQT2VWXX"
 };
 
-// Inisialisasi Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
